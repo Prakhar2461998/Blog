889becab5ae26c4b70a03b12ffb6f9c85ee5f875
Prakhar2461998 <prakharsrivastava2461997@gmail.com>
2019-12-03 13:00:01 +0530

    Add Post Update

---
+++ src/components/Postform.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+
+    constructor(props)
+    {
+        super(props)
+        this.state ={};
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <form>
+                <div>
+                <label> Title :</label> <br />
+                <input type="text" name="title" />
+                </div>
+
+
+                <div>
+                <label> Body :</label> <br />
+                <textarea name="body"/>
+                </div>
+
+                <br />
+                <button>ADD</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default Postform;
