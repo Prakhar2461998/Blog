ed8e9821c33ac79370f56c4c935bc3039b6b1c93
Prakhar2461998 <prakharsrivastava2461997@gmail.com>
2019-12-04 08:27:36 +0530

    Store updation

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,33 +1,80 @@
 import React, { Component } from 'react'
 
-class Postform extends Component {
+ class PostForm extends Component {
+
 
     constructor(props)
     {
-        super(props)
-        this.state ={};
+          super(props)
+         this.state={
+
+           Title:'',
+           body:' '
+         }
+
+        this.onChange = this.onChange.bind(this);
+
+        this.onSubmit = this.onSubmit.bind(this);
     }
+
+    onChange(e)
+    {
+           this.setState({ [e.target.name]:e.target.value});
+
+    }
+
+    onSubmit(e)
+    {
+
+            e.preventDefault();
+            const post={
+                Title:this.state.Title,
+                body:this.state.body
+            }   
+
+            fetch('https://jsonplaceholder.typicode.com/posts' , {
+               
+            method:'POST',
+            headers:{
+                'content-type': 'application/json'   
+            },
+            body: JSON.stringify(post)
+            })
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
+
+
     render() {
         return (
             <div>
-                <h1>Add Post</h1>
-                <form>
-                <div>
-                <label> Title :</label> <br />
-                <input type="text" name="title" />
+                <h1>Add Post </h1>
+  <form  onSubmit={this.onSubmit}>
+    <div>
+    <label>
+    Title :
+    </label><br />
+    <input type="text" name="Title"  onChange={this.onChange} value={this.state.title} />
+    </div>
+
+    
+    <div>
+    <br />
+    <label>
+    Body:
+    </label><br />
+    <textarea name="body"  onChange={this.onChange} value={this.state.body}/>
+    </div>
+
+    <br />
+    <button type="submit">Submit</button>
+    <button></button>
+  
+  </form>
+
                 </div>
-
-
-                <div>
-                <label> Body :</label> <br />
-                <textarea name="body"/>
-                </div>
-
-                <br />
-                <button>ADD</button>
-                </form>
-            </div>
         )
     }
 }
-export default Postform;
+
+export default PostForm;
